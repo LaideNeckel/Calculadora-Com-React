e542ceef006116d8e97211c9e1e3d5b2ca9ec0de
LaideNeckel <laidelurdes@gmail.com>
2023-01-31 13:59:11 -0300

    Calculadora React

---
+++ src/components/Button/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+
+export const ButtonContainer = styled.button `
+    padding: 20px;
+    border: 1px solid #CDCDCD;
+    border-radius: 15px;
+    background-color: #002F4D;
+    color: #FFFFFF;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+
+    &: hover {
+        opacity: 0.7;
+    }
+`
